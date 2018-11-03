9fe7edbda02b3b17c3cee1078f44e684669d87a5
Yanissa08 <44710168+Yanissa08@users.noreply.github.com>
2018-11-02 23:18:28 -0500

    Update Proyecto pruebas2-C&Y.c
    Co-Authored-By: carolcm03 <carolcm03@users.noreply.github.com>

--- Proyecto pruebas2-C&Y.c
+++ Proyecto pruebas2-C&Y.c
@@ -1,8 +1,7 @@
  #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- //listo ya
- //YA VOY A DORMIR
+ //que tengas pesadilllas
 char matrizpo1 [8][8];
 char matrizpo2 [8][8];
 char matar1 [8][8];
